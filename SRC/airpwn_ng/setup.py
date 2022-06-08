922493fec9aa88a4a3b67006c9ac899ba6554662
stryngs <info@ethicalreporting.org>
2022-06-08 03:32:22 -0400

    pip sync 2.0.4

--- SRC/airpwn_ng/setup.py
+++ SRC/airpwn_ng/setup.py
@@ -4,12 +4,14 @@ from setuptools import setup
 
 setup(
     name = 'airpwn-ng',
-    version = '2.0',
+    version = '2.0.4',
     author = 'stryngs and Jack64',
     packages = ['airpwn_ng', 'airpwn_ng.lib'],
     include_package_data = True,
     url = 'https://github.com/ICSec/airpwn-ng',
     license ='GNU General Public License v3',
     keywords = '802.11 Packet Injection',
-    description='Packet injection for wifi; simplified.'
+    description='Packet injection for wifi; simplified.',
+    long_description = 'Packet injection for wifi; simplified.',
+    install_requires = ['scapy==2.4.5']
 )
